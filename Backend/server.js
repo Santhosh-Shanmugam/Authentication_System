679c00fe0d41c04d4c5c8df0fe827402ce0e5c41
Santhosh Shanmugam <santhoshs192005@gmail.com>
2025-06-29 20:33:18 +0530

    Feat : DB connects

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import { connectDB } from './config/ConnectDB.js';
+
 
 const app = express();
 dotenv.config()
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.get('/',(req,res)=>{
     res.send("Server Starts....");
 })
+
+connectDB();
 
 app.listen(5000,()=>{
     console.log(`Server listens at port: ${PORT}`)
