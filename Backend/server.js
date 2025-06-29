8d1e8d81c3954716639e1c8d0475b96ce66b5127
Santhosh Shanmugam <santhoshs192005@gmail.com>
2025-06-29 20:14:46 +0530

    Feat : Server setuo

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv'
+
+const app = express();
+dotenv.config()
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    res.send("Server Starts....");
+})
+
+app.listen(5000,()=>{
+    console.log(`Server listens at port: ${PORT}`)
+})
